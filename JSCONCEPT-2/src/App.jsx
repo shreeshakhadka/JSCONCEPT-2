8b64690d3e184adb1bbbb260bb8cbf539a9adef8
shreeshakhadka <khadkashreesha08@gmail.com>
2023-07-05 15:39:52 -0400

    JSCONCEPT-2 initial commit

---
+++ JSCONCEPT-2/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const products = [
+    { name: 'Pants', price: 25, quantity: 17 },
+    { name: 'Tops', price: 17, quantity: 8 },
+    { name: 'Dress', price: 38, quantity: 12 }
+  ];
+
+  function calculateTotalPrice(product){
+    return product.price * product.quantity;
+  }
+
+  function printProductInfo(product) {
+    console.log('Product Name:', product.name);
+    console.log('Product Quantity:', product.quantity);
+    console.log('Total Price:', calculateTotalPrice(product));
+    
+  }
+  printProductInfo(products[1])
+  
+  return (
+    <div>
+      <h1>Product Details</h1>
+      {products.map((product, index) => (
+        <div key={index}>
+          <h2>{product.name}</h2>
+          <p>Price: {product.price}</p>
+          <p>Quantity: {calculateTotalPrice(product)}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
